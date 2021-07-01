8dd580fd3e1717bf884d368a6d53e0fd26f64855
Jonathan Yu <jonyu02@gmail.com>
2021-07-01 01:08:00 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Jonathan."
+    end
+
+    get '/hometown' do
+        "My hometown is Scotch Plains."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is \"Feeling Good\" by Michael Bublé."
+    end
+
 end
